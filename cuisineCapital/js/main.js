47ee838dbf1d56fb4e18c3dcd72300d939ef2386
Mustapha BKL <m.boukali@void.fr>
2019-06-28 18:33:00 +0100

    update template

--- cuisineCapital/js/main.js
+++ cuisineCapital/js/main.js
@@ -9,10 +9,41 @@
     });
 
 
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
+// scroll rendre e nav bar transpar
+
+window.onscroll = () => {
+  const nav = document.querySelector('.navbar');
+  if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light sticky-top '; 
+  else nav.className = 'navbar navbar-expand-lg navbar-light bg-light sticky-top stickk';
 };
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+
+
+//Le Zome img sur un pop-up
+
+$(document).ready(function() {
+  var modal = $('#myModal')
+  var span = $(".close")  
+  var modalImg = $("#img01")
+  var captionText = $("#caption")
+ 
+  var img = $('.myImg')
+ 
+ img.click(function(){
+      modal.css('display', 'block')
+      modalImg.attr('src', this.src)
+      captionText.html(this.alt)
+  });
+ 
+  span.click(function() {
+    modal.css('display', 'none')
+	});
+});
+
+//   hover and zomm image 
+$(document).ready(function(){
+  $('img')
+    .wrap('<span style="display:inline-block"></span>')
+    .css('display', 'block')
+    .parent()
+    .zoom();
+});
